44cb2ce726f07f5eceba42391ed92770e3336330
Tran Kim Truong <truongprobk1992@gmail.com>
2015-08-07 18:05:14 +0700

    Tasker notification

--- client/tasker/task-list-notification.jsx
+++ client/tasker/task-list-notification.jsx
@@ -6,6 +6,8 @@
 const{
   AppBar,
   IconButton,
+  MenuItem,
+  LeftNav
 } = mui;
 
 // var customPalette = {
@@ -15,6 +17,28 @@ const{
 
 const ThemeManager = new mui.Styles.ThemeManager();
 //ThemeManager.setPalette(customPalette);
+menuItems = [
+  { route: 'get-started', text: 'Get Started' },
+  { route: 'customization', text: 'Customization' },
+  { route: 'components', text: 'Components' },
+  { type: MenuItem.Types.SUBHEADER, text: 'Resources' },
+  {
+     type: MenuItem.Types.LINK,
+     payload: 'https://github.com/callemall/material-ui',
+     text: 'GitHub'
+  },
+  {
+     text: 'Disabled',
+     disabled: true
+  },
+  {
+     type: MenuItem.Types.LINK,
+     payload: 'https://www.google.com',
+     text: 'Disabled Link',
+     disabled: true
+  },
+];
+
 
 ListTaskNotification = React.createClass({
   childContextTypes: {
@@ -34,6 +58,11 @@ ListTaskNotification = React.createClass({
   propTypes: {
   },
 
+  onClickNotification(e) {
+    console.log(e);
+    this.refs.leftNav.toggle();
+  },
+
   render() {
     return (
       <div>
@@ -41,12 +70,19 @@ ListTaskNotification = React.createClass({
           title="Danh sách công việc"
           iconElementRight={
             <div>
-              <IconButton iconClassName="icon-notification" />
+              <IconButton
+                iconClassName="icon-notification"
+                onFocus={this.onClickNotification}/>
               <IconButton iconClassName="icon-help" />
               <IconButton iconClassName="icon-back" />
             </div>
           } />
           <div id="main">
+            <LeftNav
+              ref="leftNav"
+              docked={false}
+              menuItems={menuItems}
+              openRight={true}/>
             <p>----------------------------</p>
             <br/><br/><br/><br/><br/>
             <p>----------------------------</p>
